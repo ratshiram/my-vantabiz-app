ef2ac39fe435793cb7a222057701b116a7828eb0
Evergreentots Daycare <evergreentotsdaycare@gmail.com>
2025-06-15 11:15:38 +0000

    please make the logo original size

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,13 +5,12 @@ import Image from 'next/image';
 export function AppHeader() {
   return (
     <header className="flex flex-col items-center py-6 sm:py-8 bg-card border-b border-border">
-      <div className="relative w-auto h-24 sm:h-28">
+      <div className="relative">
         <Image
           src="/vantabizlogo.png"
           alt="VantaBiz Logo"
           width={500}
           height={500}
-          style={{ objectFit: 'contain', width: 'auto', height: '100%' }}
           priority
         />
       </div>
