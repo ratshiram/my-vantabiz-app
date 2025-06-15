d612ee3fa2eafd8b2da92044d1f5cdf9ac9ccc3c
Evergreentots Daycare <evergreentotsdaycare@gmail.com>
2025-06-15 10:51:33 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+        'http://6000-firebase-studio-1749937401269.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev'
+    ],
+  }
+};
+
+export default nextConfig;
